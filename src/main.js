380e95d36737f870cad378dc59a30e447d97e21d
csyuser <13540039093@163.com>
2021-01-05 10:41:56 +0800

    初始化项目&&引入arcgis

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import '@arcgis/core/assets/esri/themes/dark/main.css';
 
 Vue.config.productionTip = false
 
