91011adaffcb08d2d85029590e017aa9c9620ad2
csyuser <13540039093@163.com>
2021-01-05 17:59:55 +0800

    第一个例子ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="mapdiv">
+      <div class="home" ref="home"></div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView";
+import Bookmarks from "@arcgis/core/widgets/Bookmarks";
+import Expand from "@arcgis/core/widgets/Expand";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'App',
+  async mounted() {
+    console.log('this.$el')
+    console.log(this.$el)
+    const webmap = new WebMap({
+      portalItem: {
+        id: "aa1d3f80270146208328cf66d022e09c",
+      },
+    });
+    const view = new MapView({
+      container: this.$refs.home,
+      map: webmap,
+    });
+    const bookmarks = new Bookmarks({
+      view: view,
+      // allows bookmarks to be added, edited, or deleted
+      editingEnabled: true,
+    });
+    const bkExpand = new Expand({
+      view: view,
+      content: bookmarks,
+      expanded: true,
+    });
+    // Add the widget to the top-right corner of the view
+    view.ui.add(bkExpand, "top-right");
+    // bonus - how many bookmarks in the webmap?
+    webmap.when(function () {
+      if (webmap.bookmarks && webmap.bookmarks.length) {
+        console.log("Bookmarks: ", webmap.bookmarks.length);
+      } else {
+        console.log("No bookmarks in this webmap.");
+      }
+    });
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home{
+  height: 300px;
+  width: 1000px;
+}
+</style>
